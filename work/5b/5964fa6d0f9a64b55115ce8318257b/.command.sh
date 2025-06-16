11368412882cbf2ea948388f9e47c57c1286b4f9
antoine-buetti <antoine.buetti@gmail.com>
2025-06-16 11:52:18 +0000

    Update info

--- work/5b/5964fa6d0f9a64b55115ce8318257b/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-mkdir "UFO_Kalmar_20140401_frames"
-mplayer -nosound -vo jpeg:outdir="UFO_Kalmar_20140401_frames" -speed 100 "UFO_Kalmar_20140401.mp4" -benchmark
--- work/8d/baf31220421a9b9bdaa82b0fdf9e78/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-mkdir "vid2_frames"
-mplayer -nosound -vo jpeg:outdir="vid2_frames" -speed 100 "vid2.mp4" -benchmark
